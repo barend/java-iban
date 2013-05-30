656afb61071489006d5362d35f3e10cf54a4cbe1
Barend Garvelink <bgarvelink@xebia.com>
2013-05-30 21:20:33 +0200

    Expand sample code in README and package-info.

--- src/main/java/nl/garvelink/iban/package-info.java
+++ src/main/java/nl/garvelink/iban/package-info.java
@@ -25,26 +25,37 @@
  * // Obtain an instance of IBAN.
  * IBAN iban = IBAN.valueOf( "NL91ABNA0417164300" );
  *
+ * // toString() emits standard formatting, toPlainString() is compact.
+ * String s = iban.toString(); // "NL91 ABNA 0417 1643 00"
+ * String p = iban.toPlainString(); // "NL91ABNA0417164300"
+ *
  * // Input may be formatted.
  * iban = IBAN.valueOf( "BE68 5390 0754 7034" );
  *
  * // The valueOf() method returns null if its argument is null.
- * iban.valueOf( null );
+ * iban.valueOf( null ); // null
  *
  * // The parse() method throws an exception if its argument is null.
- * iban.parse( null ); // Kaboom.
+ * iban.parse( null ); // IllegalArgumentException
  *
- * // IBAN does not implement Comparable&lt;T&gt;, but a simple Comparator is provided.
- * List&lt;IBAN&gt; ibans = getListOfIBANs();
+ * // IBAN does not implement Comparable<T>, but a simple Comparator is provided.
+ * List<IBAN> ibans = getListOfIBANs();
  * Collections.sort( ibans, IBAN.LEXICAL_ORDER );
  *
- * // You can use the Modulo97 class directly to compute or verify the check digits.
- * String candidate = "GB29 NWBK 6016 1331 9268 19";
- * Modulo97.verifyCheckDigits( candidate );
+ * // The equals() and hashCode() methods are implemented.
+ * Map<IBAN, String> ibansAsKeys = Maps.newHashMap();
+ * ibansAsKeys.put( iban, "this is fine" );
  *
- * // API methods take CharSequence, not just String.
- * StringBuilder builder = new StringBuilder( "LU280019400644750000" );
- * int checkDigits = Modulo97.calculateCheckDigits( builder );
+ * // You can use the Modulo97 class directly to compute or verify the check digits on an input.
+ * String candidate = "GB29 NWBK 6016 1331 9268 19";
+ * boolean valid = Modulo97.verifyCheckDigits( candidate ); // true
+ *
+ * // Modulo97 API methods take CharSequence, not just String.
+ * StringBuilder builder = new StringBuilder( "LU000019400644750000" );
+ * int checkDigits = Modulo97.calculateCheckDigits( builder ); // 28
+ *
+ * // Get the expected IBAN length for a country code:
+ *  int length = IBAN.getLengthForCountryCode( "DK" );
  * </pre>
  *
  * <p>Copyright 2013 Barend Garvelink. This code can be used under the terms of the Apache License, Version 2.0.
